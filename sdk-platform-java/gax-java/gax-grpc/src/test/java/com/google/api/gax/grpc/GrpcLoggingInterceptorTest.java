aa87ad86f85c6e25bb5cce014dff25bceb12916b
Nidhi Nandwani <nidhiii@google.com>
2026-07-29 06:44:45 +0000

    fix formatting

--- sdk-platform-java/gax-java/gax-grpc/src/test/java/com/google/api/gax/grpc/GrpcLoggingInterceptorTest.java
+++ sdk-platform-java/gax-java/gax-grpc/src/test/java/com/google/api/gax/grpc/GrpcLoggingInterceptorTest.java
@@ -32,7 +32,6 @@ package com.google.api.gax.grpc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
